65846bdbf134bb4cf9d46c8417868c091ab7494b
陳禮群 <gerven11@hotmail.com>
2017-12-16 11:41:28 +0800

    20 練習 /MB/Index 頁面的批次更新功能 ( 使用多筆資料的模型繫結 )

--- MVC5Course/Controllers/MBController.cs
+++ MVC5Course/Controllers/MBController.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
+    public class MBBatchUpdateVM
+    {
+        public int ProductId { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<bool> Active { get; set; }
+        public Nullable<decimal> Stock { get; set; }
+    }
+
     public class MBController : BaseController
     {
         public ActionResult Index()
@@ -14,9 +23,42 @@ namespace MVC5Course.Controllers
 
             ViewData.Model = data;
 
-            ViewData["PageTitle"] = "商品清單";
-            //ViewBag.PageTitle = "商品清單";
+            //ViewData["PageTitle"] = "商品清單";
+            ViewBag.PageTitle = "商品清單";
+            
+            return View();
+        }
 
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Index(MBBatchUpdateVM[] batch)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach(var item in batch)
+                {
+                    var ProductData = repo.Find(item.ProductId);
+                    ProductData.Price = item.Price;
+                    ProductData.ProductId = item.ProductId;
+                    ProductData.Stock = item.Stock;
+                }
+                try
+                {
+                    repo.UnitOfWork.Commit();
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    throw ex;
+                }
+                
+
+                return RedirectToAction("Index");
+            }
+
+            var data = repo.Get取得所有尚未刪除的商品資料Top10();
+            ViewData.Model = data;
+            ViewBag.PageTitle = "商品清單";
+            
             return View();
         }
     }
