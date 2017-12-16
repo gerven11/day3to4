861b8291b62b94329a2bf8fcf176508161c93ec5
陳禮群 <gerven11@hotmail.com>
2017-12-16 14:03:52 +0800

    22 練習 Action Filter 的設計方式

---
+++ MVC5Course/ActionFilter/LocalOnlyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.ActionFilter
+{
+    public class LocalOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.RequestContext.HttpContext.Request.IsLocal)
+            {
+                filterContext.Result = new RedirectResult("/");
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ MVC5Course/ActionFilter/ShareDataAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+/// <summary>
+/// 建立ActionFilter
+/// 1. 建立一個ActionFilter 資料夾
+/// 2. 建立共用的Attributes 類別(Class)，要繼承 ActionFilterAttribute
+/// 3.在controller上套用
+/// </summary>
+namespace MVC5Course.ActionFilter
+{
+    public class ShareDataAttribute: ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.Message = "Your application description page.";
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
