140d2e93f8996ae7ad18804e6dafefc49bf1db06
陳禮群 <gerven11@hotmail.com>
2017-12-17 12:58:08 +0800

    26 練習 @Html.Action 將複雜的 View 頁面切割成多個部分

---
+++ MVC5Course/Controllers/OrderLinesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+using System.Data.Entity;
+
+namespace MVC5Course.Controllers
+{
+    public class OrderLinesController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+        // GET: OrderLines
+        public ActionResult Index(int id)
+        {
+            var orderLine = db.OrderLine
+                .Where(p => p.ProductId == id)
+                .Include(o => o.Order).Include(o => o.Product);
+            return View(orderLine.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
